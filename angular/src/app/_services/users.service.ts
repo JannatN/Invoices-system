23f33aa10bd7a125e65ee6431570b5573f04a661
JannatN <jannatfaisalnatsheh@gmail.com>
2021-06-24 23:34:43 +0300

    Admin users list

---
+++ angular/src/app/_services/users.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+
+  private baseUrl = 'http://localhost:8080/api/v1/users';
+
+  constructor(private http: HttpClient) { }
+
+  getUsers(id: number): Observable<any> {
+    return this.http.get(`${this.baseUrl}/${id}`);
+  }
+
+  updateUser(id: number, value: any): Observable<Object> {
+    return this.http.put(`${this.baseUrl}/${id}`, value);
+  }
+
+  getUserList(): Observable<any> {
+    return this.http.get(`${this.baseUrl}`);
+  }
+}
