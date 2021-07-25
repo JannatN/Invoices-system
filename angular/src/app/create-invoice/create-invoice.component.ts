350c9752d36d884dcb670f0a26739d4c0f57b8db
JannatN <jannatfaisalnatsheh@gmail.com>
2021-07-26 00:12:07 +0300

    Create invoice with more than 1 item

--- angular/src/app/create-invoice/create-invoice.component.ts
+++ angular/src/app/create-invoice/create-invoice.component.ts
@@ -3,10 +3,9 @@ import { Component, OnInit, Input } from '@angular/core';
 import { InvoiceService } from "../_services/invoices.service";
 import { ActivatedRoute, Router } from '@angular/router';
 import { Item } from '../models/item';
-import { Observable } from 'rxjs';
-import { HttpEventType, HttpResponse } from '@angular/common/http';
 import { UploadFilesService } from '../_services/upload-file.service';
-import { File} from "../models/file"
+import { File } from "../models/file"
+import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-create-invoice',
@@ -14,21 +13,31 @@ import { File} from "../models/file"
   styleUrls: ['./create-invoice.component.css']
 })
 export class CreateInvoiceComponent implements OnInit {
+  dynamicForm: FormGroup;
+  submitted = false;
 
   invoice: Invoice = new Invoice();
-  submitted = false;
-  // invoices: Invoice;
   item: Item = new Item();
   location: any;
-  files: File[];
-  constructor(private invoiceService: InvoiceService,
+  array: [];
+
+  constructor(private formBuilder: FormBuilder, private invoiceService: InvoiceService,
     private router: Router, private route: ActivatedRoute, private uploadService: UploadFilesService) { }
 
   ngOnInit() {
-    // this.fileInfos = this.uploadService.getFiles();
+    this.dynamicForm = this.formBuilder.group({
+      numberOfItems: ['', Validators.required],
+      items: new FormArray([])
+    });
 
   }
+  get f() {
+    return this.dynamicForm.controls;
+  }
+  get t() {
+    return this.f.items as FormArray;
 
+  }
   saveInvoice() {
     this.invoiceService.createInvoice(this.invoice).subscribe(data1 => {
       console.log(data1)
@@ -36,19 +45,66 @@ export class CreateInvoiceComponent implements OnInit {
     })
   }
 
-  onSubmit() {
-    this.invoice.items = [];
-    this.invoice.items.push(this.item);
-    // this.invoice.files = this.files;
-    // this.invoice.files.push(this.files);
-    // this.uploadFiles();
+  onChangeItems(e) {
+    const numberOfItems = e.target.value || 0;
+    if (this.t.length < numberOfItems) {
+      for (let i = this.t.length; i < numberOfItems; i++) {
+        this.t.push(this.formBuilder.group({
+          name: ['', Validators.required],
+          description: ['', Validators.required],
+          price: ['', Validators.required],
+          currency: ['', Validators.required],
+          quantity: ['', Validators.required],
+        }));
+      }
+      this.invoice.items = [];
+      for (let i = this.t.length; i < numberOfItems; i++) {
+        this.invoice.items.push(this.dynamicForm.value.items[i]);
 
-    // console.log("array item", this.item)
-    // console.log("array file", this.invoice.files)
+      }
+      
+    } else {
+      for (let i = this.t.length; i >= numberOfItems; i--) {
+        this.t.removeAt(i);
+      }
+    }
+  }
+
+
+  onSubmit() {
     this.submitted = true;
+    this.invoice.items = [];
+    for (let i = 0; i < 10; i++) {
+      this.invoice.items.push(this.dynamicForm.value.items[i]);
+    }
+
+    console.log("items", this.invoice);
+    console.log("values", this.dynamicForm.value.items);
+
+
     this.saveInvoice();
     console.log("invoice created");
+    if (this.dynamicForm.invalid) {
+      return;
+    }
 
+    // display form values on success
+    alert('SUCCESS!! :-)\n\n' + JSON.stringify(this.dynamicForm.value, null, 4));
+
+
+  }
+
+  onReset() {
+    // reset whole form back to initial state
+    this.submitted = false;
+    this.dynamicForm.reset();
+    this.t.clear();
+  }
+
+  onClear() {
+    // clear errors and reset ticket fields
+    this.submitted = false;
+    this.t.reset();
   }
 
   gotoList() {
